515ecded12d638ea7a8fa367639f4a65cc29979e
MISSQINWSL <1138027375@qq.com>
2021-12-02 00:03:33 +0800

    feat: 匹配路由路径

--- my_vue_router/create-matcher.js
+++ my_vue_router/create-matcher.js
@@ -1,4 +1,5 @@
 import createRouteMap from './create-route-map';
+import createRoute from './util/route';
 
 /**
  * 解析路由规则
@@ -10,7 +11,14 @@ export default function createMatcher(router) {
 
     // match 从 pathMap 中获取 path 对应的路由记录
     function match(path) {
+        // 获取 path 对应的记录
+        const record = pathMap[path];
+        // 如果记录存在的话就进行遍历
+        if (record) {
+            return createRoute(record, path);
+        }
 
+        return createRoute(null, path);
     }
 
     // 添加动态路由
