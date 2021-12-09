b165c91770c400edbafd7ab7ade7edfe459b11e5
MISSQINWSL <1138027375@qq.com>
2021-12-09 22:50:26 +0800

    feat: 初始化 vuex

---
+++ my_vuex/index.js
@@ -0,0 +1,20 @@
+// 保存全局的 Vue
+let _Vue = null;
+
+// Store 类
+class Store {
+    constructor(options) {
+        
+    }
+}
+
+// install 方法负责将 Vuex 可以被 Vue.use() 安装
+function install(Vue) {
+
+}
+
+// 导出 install 和 Store
+export default {
+    install,
+    Store,
+}
